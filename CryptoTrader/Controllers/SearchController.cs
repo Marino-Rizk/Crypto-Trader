f4c00703104cc4fe9e531998c64035da710ff07f
Marino-Rizk <marinorizk@gmail.com>
2025-05-15 20:34:42 +0300

    Initial commit

---
+++ CryptoTrader/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoTrader.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index(string query)
+        {
+            return View();
+        }
+    }
+}
